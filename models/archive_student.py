7af133046ff70f8609cfc66fa2aedb11a7a744ea
Bravin6 <bravinwekhuyi@gmail.com>
2024-07-14 06:22:33 -0700

    models

--- models/archive_student.py
+++ models/archive_student.py
@@ -1,1 +1,14 @@
-  
+from flask_login import UserMixin
+from models.user import db
+from datetime import datetime
+
+
+class Student(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=True, nullable=False)
+    last_name = db.Column(db.String(150), unique=True, nullable=False)
+    student_id = db.Column(db.Integer, nullable=False)
+    department = db.Column(db.String, nullable=False)
+    DOB = db.Column(db.string, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now)  
